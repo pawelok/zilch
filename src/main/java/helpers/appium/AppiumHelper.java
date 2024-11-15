074c6a0506ea0c6d7ca7213d327371f38941c260
pawelok <paweg50@gmail.com>
2024-11-15 09:08:33 +0100

    added platforms helpers

--- src/main/java/helpers/appium/AppiumHelper.java
+++ src/main/java/helpers/appium/AppiumHelper.java
@@ -1,18 +1,16 @@
-package helpers;
+package helpers.appium;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.spi.LoggerContextFactory;
-import org.junit.platform.commons.logging.LoggerFactory;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 
 public class AppiumHelper {
     private static final Logger LOG = LogManager.getLogger(AppiumHelper.class);
 
     public boolean isElementDisplayed(WebElement element) {
-        int POOL_DELAY_MS = 100; // Delay between checks in milliseconds
-        int MAX_ATTEMPTS = 10;  // Max number of attempts
+        int POOL_DELAY_MS = 100;
+        int MAX_ATTEMPTS = 5;
+
         for (int i = 0; i < MAX_ATTEMPTS; i++) {
             try {
                 if (element.isDisplayed()) {
@@ -20,13 +18,13 @@ public class AppiumHelper {
                     return true;
                 }
             } catch (Exception e) {
-                LOG.error("Attempt " + (i + 1) + ": Error" + e.getMessage());
+                LOG.error("Attempt " + (i + 1) + ": Error " + e.getMessage());
             }
             try {
                 Thread.sleep(POOL_DELAY_MS);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
-                LOG.error("Attempt " + (i + 1) + ": Error" + e.getMessage());
+                LOG.error("Attempt " + (i + 1) + ": Error " + e.getMessage());
                 break;
             }
         }
---
+++ src/main/java/helpers/appium/PlatformInterface.java
@@ -0,0 +1,12 @@
+package helpers.appium;
+
+public interface PlatformInterface {
+
+    boolean isKeyboardDisplayed();
+
+    void hideKeyboard();
+
+    void pressOnReturnKey();
+
+    void  restartApp(String appId, String appActivity);
+}
