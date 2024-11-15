38ee46ae15ffdb885d63b1c0d927eec486076b04
pawelok <paweg50@gmail.com>
2024-11-15 10:20:59 +0100

    typo corrections

--- src/test/java/api/ApiTests.java
+++ src/test/java/api/ApiTests.java
@@ -10,11 +10,11 @@ public class ApiTests {
     long maxResponseTimeoutMs = 1000;
     String sampleUiToken = "1571807edab54ec89.1442995705|r=eu-west-1|meta=3|meta_width=335|meta_height=431|metabgclr=transparent|metaiconclr=%23757575|guitextcolor=%23000000|pk=D5264E07-85CF-434C-88E5-6F095A832C01|at=40|ag=101|cdn_url=https%3A%2F%2Fclient-api.arkoselabs.com%2Fcdn%2Ffc|lurl=https%3A%2F%2Faudio-eu-west-1.arkoselabs.com|surl=https%3A%2F%2Fclient-api.arkoselabs.com|smurl=https%3A%2F%2Fclient-api.arkoselabs.com%2Fcdn%2Ffc%2Fassets%2Fstyle-manager";
     String testEmail = "test@test.com";
-    String tesPassword = "88888888";
+    String testPassword = "88888888";
 
     @Test
     public void verifyIfServiceIsOnline() {
-        Response res = ApiHelper.postCreateCustomer(testEmail, tesPassword, sampleUiToken);
+        Response res = ApiHelper.postCreateCustomer(testEmail, testPassword, sampleUiToken);
         Assertions.assertAll(
                 () -> Assertions.assertFalse(res.statusCode() >= 500, "Server is probably offline. Status: " + res.statusCode()),
                 () -> Assertions.assertTrue(res.getTime() <= maxResponseTimeoutMs, "Response was taking longer than " + res.getTime() + "ms")
@@ -23,7 +23,7 @@ public class ApiTests {
 
     @Test
     public void postCreateUserWithIncorrectUiToken() {
-        Response res = ApiHelper.postCreateCustomer(testEmail, tesPassword, sampleUiToken);
+        Response res = ApiHelper.postCreateCustomer(testEmail, testPassword, sampleUiToken);
         Assertions.assertAll(
                 () -> Assertions.assertEquals(403, res.statusCode()),
                 () -> Assertions.assertTrue(res.getTime() <= maxResponseTimeoutMs, "Response was taking longer than " + res.getTime() + "ms")
@@ -32,7 +32,7 @@ public class ApiTests {
 
     @Test
     public void postCreateUserWithEmptyUiToken() {
-        Response res = ApiHelper.postCreateCustomer(testEmail, tesPassword, "");
+        Response res = ApiHelper.postCreateCustomer(testEmail, testPassword, "");
         Assertions.assertAll(
                 () -> Assertions.assertEquals(403, res.statusCode()),
                 () -> Assertions.assertTrue(res.getTime() <= maxResponseTimeoutMs, "Response was taking longer than " + res.getTime() + "ms")
